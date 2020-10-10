1a09a747d446fae932049213ecd54747a4d61c87
MNDonut <61380547+MNDonut@users.noreply.github.com>
2020-10-10 22:54:27 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,4 +5,6 @@
   $email = $_POST['email'];
   mail ($to, "Повідомлення з сайту від ".$name." <$email>", $message, "Content-type: text/html; charset=utf-8");
   mail ($email, "XGame Project", "Хочемо вам повідомити, що ваше повідомлення було відправлено та буде розглянуте протягом двох днів. Спасибі за вашу активність :)", "Content-type: text/html; charset=utf-8");
+  header("Location: https://projectxgame.000webhostapp.com/");
+  exit;
 ?>
