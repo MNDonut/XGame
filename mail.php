9a3a11efb07a23b604444f9c50040d41aee17633
MNDonut <61380547+MNDonut@users.noreply.github.com>
2020-10-10 22:50:10 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -4,4 +4,5 @@
   $name = $_POST['name'];
   $email = $_POST['email'];
   mail ($to, "Повідомлення з сайту від ".$name." <$email>", $message, "Content-type: text/html; charset=utf-8");
+  mail ($email, "XGame Project", "Хочемо вам повідомити, що ваше повідомлення було відправлено та буде розглянуте протягом двох днів. Спасибі за вашу активність :)", "Content-type: text/html; charset=utf-8");
 ?>
